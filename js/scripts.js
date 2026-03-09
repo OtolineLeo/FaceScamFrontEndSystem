97b542ec7510c732dbf45d173c87494b0adac66f
unknown <carlosotoline@gmail.com>
2026-03-09 13:44:03 -0300

    adding facescan

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+const video = document.getElementById('myVideo');
+const butao = document.getElementById('myBtn');
+
+myBtn.addEventListener("click", () => {
+
+    if(!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia){
+        alert('Seu navegador não suporta este recurso');
+        return;
+    }
+
+    navigator.mediaDevices.getUserMedia({video: true}).
+    then(function (stream) {
+        video.srcObject = stream;
+        video.onplay();
+    })
+    .catch(function(err){
+        console.log("Erro: " + err);
+    });
+})
