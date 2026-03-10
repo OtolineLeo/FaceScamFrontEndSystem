0132d0027d13314c47ea157a7771cfd95fa04fe0
unknown <carlosotoline@gmail.com>
2026-03-10 20:05:34 -0300

    last att and adding mic

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,17 @@ const butao = document.getElementById('myBtn');
 
 myBtn.addEventListener("click", () => {
 
+    // mediaDevices é uma api que permite acessar camera, mic e tela
+    // getUserMedia é a operação que pede acesso à camera ou mix
+
     if(!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia){
         alert('Seu navegador não suporta este recurso');
         return;
     }
 
-    navigator.mediaDevices.getUserMedia({video: true}).
+    // getUserMedia aqui é uma operação que vai terminar no futuro
+
+    navigator.mediaDevices.getUserMedia({video: true, audio: true}).
     then(function (stream) {
         video.srcObject = stream;
         video.play();
